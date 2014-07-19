afd5d86f5f73a51aed1411880d4c748d448954c5
Damián Favela <odissey@gmail.com>
2014-07-19 05:38:43 -0500

    Se agregaron todos los puntos del temario
    Se agregaron todos los puntos del temario

--- src/com/example/googlemapsexample/MainActivity.java
+++ src/com/example/googlemapsexample/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.googlemapsexample;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import android.app.Activity;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -15,28 +15,27 @@ public class MainActivity extends FragmentActivity {
 	
 	GoogleMap googleMap;
 
-    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB) @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        googleMap = mapFragment.getMap();
- 
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         
-//        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-//        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        
-        
-        googleMap.setMyLocationEnabled(true);
-        
-        final LatLng Pune = new LatLng(18.500486, 73.866899);
-        Marker pune = googleMap.addMarker(new MarkerOptions()
-                                  .position(Pune)
-                                  .title("Pune")
-                                  .snippet("Population: 54,137,400")
-                                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+//      googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//      googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+      googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+      
+      
+      googleMap.setMyLocationEnabled(true);
+      
+      final LatLng Pune = new LatLng(18.500486, 73.866899);
+      Marker pune = googleMap.addMarker(new MarkerOptions()
+                                .position(Pune)
+                                .title("Pune")
+                                .snippet("Population: 54,137,400")
+                               .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
         
     }
+    
 }
