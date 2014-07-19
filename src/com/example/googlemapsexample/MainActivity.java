ae046a6ae69e830aa8dc3fede02f68ce5cbaf225
Damián Favela <odissey@gmail.com>
2014-07-19 05:06:28 -0500

    Se agregaron todos los puntos del temario
    Se agregaron todos los puntos del temario

--- src/com/example/googlemapsexample/MainActivity.java
+++ src/com/example/googlemapsexample/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.googlemapsexample;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+	
+	GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        googleMap = mapFragment.getMap();
+ 
+        
+//        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        
+        
+        googleMap.setMyLocationEnabled(true);
+        
+        final LatLng Pune = new LatLng(18.500486, 73.866899);
+        Marker pune = googleMap.addMarker(new MarkerOptions()
+                                  .position(Pune)
+                                  .title("Pune")
+                                  .snippet("Population: 54,137,400")
+                                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        
     }
 }
